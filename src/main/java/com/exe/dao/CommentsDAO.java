f4a198880e0f7eb4a67736565888305bf205ccc9
climaxna <climaxna@naver.com>
2015-01-20 12:17:47 +0900

    재영+신진

---
+++ src/main/java/com/exe/dao/CommentsDAO.java
@@ -0,0 +1,39 @@
+package com.exe.dao;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.exe.dto.CommentsDTO;
+
+public class CommentsDAO {
+	
+private SqlSessionTemplate sessionTemplate;
+	
+	@Autowired
+	public void setSessionTemplate(SqlSessionTemplate sessionTemplate) {
+		this.sessionTemplate = sessionTemplate;
+	}
+	
+	
+	public int cmMaxNum(){
+		int result = 0;
+		
+		result = sessionTemplate.selectOne("comments.cmMaxNum");
+		return result;
+	}
+	
+	public void cmInsert(CommentsDTO dto){
+		
+		sessionTemplate.insert("comments.cmInsert", dto);
+		
+	}
+	
+	public List<CommentsDTO> cmList(int brNum) {
+		List<CommentsDTO> lists = sessionTemplate.selectList("comments.cmList", brNum);
+		return lists;
+	}
+
+
+}
---
+++ src/main/java/com/exe/dao/HistoryDAO.java
@@ -0,0 +1,36 @@
+package com.exe.dao;
+
+import java.nio.channels.SeekableByteChannel;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.exe.dto.HistoryDTO;
+
+public class HistoryDAO {
+	
+	private SqlSessionTemplate sessionTemplate;
+
+
+	public void setSessionTemplate(SqlSessionTemplate sessionTemplate) {
+		this.sessionTemplate = sessionTemplate;
+	}
+	
+	public int hsMaxNum(){
+		int result=0;
+		
+		result = sessionTemplate.selectOne("history.hsMaxNum");
+		
+		return result;
+	}
+	
+	public void hsInsert(HistoryDTO dto){
+		sessionTemplate.insert("history.hsInsert", dto);
+	}
+	
+	public List<HistoryDTO> selectHistory(String brNum){
+		List<HistoryDTO> lists = sessionTemplate.selectList("history.selectHistory",brNum);
+		return lists;
+	}
+
+}
